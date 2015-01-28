fd891f80c0a916dc7719e1443b862d0954872841
xien <xing.xie@logicmonitor.com>
2015-01-28 14:49:38 +0800

    change the directory of templates

--- bin/wrapper.sh
+++ bin/wrapper.sh
@@ -4,5 +4,4 @@
 #
 # -----------------------------------------------------------------------------
 
-echo "$java_exe" "$wrapper_java_options" "$wrapper_java_sys_options" -jar "$wrapper_jar" "$@" 
 "$java_exe" "$wrapper_java_options" "$wrapper_java_sys_options" -jar "$wrapper_jar" "$@" 
